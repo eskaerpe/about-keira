a0ffd0c779ad61f6a4f41889d206e16aece8cc48
eskaerpe <eskaerpe01@gmail.com>
2024-02-24 20:17:06 +0700

    prevent back

--- birthday/birthday-script.js
+++ birthday/birthday-script.js
@@ -1,13 +1,9 @@
-// window.onload = function () {
-//     setTimeout(function () {
-//         $("#verifModal").modal("show");
-//     }, 2000); // Delay of 2 seconds
-// };
 window.onload = function () {
     if (window.location.href.includes("keira's-birthday.html")) {
         $("#verifModal").modal("show");
     }
 };
+
 function checkVerification() {
     let invitedNames = ["feesa", "maura", "adya", "chilla", "lala", "alika", "ira", "raya", "riri", "alya", "rei", "zada", "nadira", "alya", "kirana", "nara", "opal", "ale", "hasna", "naila", "aureli", "cyndi", "kintan", "dino"];
 
@@ -17,8 +13,14 @@ function checkVerification() {
         $("#verifModal").modal("hide");
         window.location.href = "invited-only.html";
     } else {
-        // alert("Coba lagi dengan nama lain, atau kamu memang tidak di invite");
         $("#verifModal").modal("hide");
         $("#gagalModal").modal("show");
     }
+}
+
+if (window.history && window.history.pushState) {
+    window.history.pushState("forward", null, "./#");
+    window.onpopstate = function () {
+        window.location.reload();
+    };
 }
