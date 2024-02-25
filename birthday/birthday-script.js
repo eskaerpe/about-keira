84fa8e01e9f8275387bc37c16aa2c7132b1a5764
eskaerpe <eskaerpe01@gmail.com>
2024-02-25 16:01:58 +0700

    laptop: add loading after submit button clicked

--- birthday/birthday-script.js
+++ birthday/birthday-script.js
@@ -1,36 +1,44 @@
 window.onload = function () {
-    if (window.location.href.includes("keira's-birthday.html")) {
-        $("#verifModal").modal("show");
-        var assetsFolder = "assets-bd/";
-        // var assets = ["image1.jpg", "image2.jpg", "image3.jpg", "main-bg.png"];
-        var assets = ["main-bg.png"];
+	if (window.location.href.includes("keira's-birthday.html")) {
+		$("#verifModal").modal("show");
+		var assetsFolder = "assets-bd/";
+		// var assets = ["image1.jpg", "image2.jpg", "image3.jpg", "main-bg.png"];
+		var assets = ["main-bg.png"];
 
-        assets.forEach(function (asset) {
-            var img = new Image();
-            img.src = assetsFolder + asset;
-        });
-    }
+		assets.forEach(function (asset) {
+			var img = new Image();
+			img.src = assetsFolder + asset;
+		});
+	}
 };
 
 function checkVerification() {
-    let invitedNames = ["feesa", "maura", "adya", "chilla", "lala", "alika", "ira", "raya", "riri", "alya", "rei", "zada", "nadira", "alya", "kirana", "nara", "opal", "ale", "hasna", "naila", "aureli", "cyndi", "kintan", "dino"];
+	let invitedNames = ["feesa", "maura", "adya", "chilla", "lala", "alika", "ira", "raya", "riri", "alya", "rei", "zada", "nadira", "alya", "kirana", "nara", "opal", "ale", "hasna", "naila", "aureli", "cyndi", "kintan", "dino"];
 
-    var verificationNameValue = document.querySelector("#verificationName").value;
+	var verificationNameValue = document.querySelector("#verificationName").value;
 
-    if (invitedNames.includes(verificationNameValue.toLowerCase())) {
-        $("#verifModal").modal("hide");
-        window.location.href = "invited-only.html";
-    } else {
-        $("#verifModal").modal("hide");
-        $("#gagalModal").modal("show");
-    }
+	if (invitedNames.includes(verificationNameValue.toLowerCase())) {
+		$("#verifModal").modal("hide");
+		window.location.href = "invited-only.html";
+	} else {
+		$("#verifModal").modal("hide");
+		$("#gagalModal").modal("show");
+	}
 }
 
 function confirmeOrder() {
-    $("#confirmOrderModal").modal("show");
+	$("#confirmOrderModal").modal("show");
 }
 
 function submitOrder() {
-    var inputInsideFormTag = document.querySelector('input[type="submit"]');
-    inputInsideFormTag.click();
+	var inputInsideFormTag = document.querySelector('input[type="submit"]');
+
+	// Add loading for 2 seconds before submitting
+	inputInsideFormTag.disabled = true;
+	$("#confirmOrderModal").modal("hide");
+	$("#loadingModal").modal("show");
+	setTimeout(function () {
+		inputInsideFormTag.disabled = false;
+		inputInsideFormTag.click();
+	}, 2000);
 }
